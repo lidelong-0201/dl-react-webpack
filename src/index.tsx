ba122c1a3b22c7e96d230582ae748418350fa690
lidelong <lidelong0201@foxmail.com>
2024-01-16 17:18:30 +0800

    feat: 代码质量和git提交规范

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-console.log('NODE_ENV', process.env.NODE_ENV);
-console.log('BASE_ENV', process.env.BASE_ENV);
-console.log('process.env', process.env);
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('BASE_ENV', process.env.BASE_ENV)
+console.log('process.env', process.env)
 
-const root = document.querySelector('#root');
+const root = document.querySelector('#root')
 
 if (root) {
-  createRoot(root).render(<App />);
+  createRoot(root).render(<App />)
 }
